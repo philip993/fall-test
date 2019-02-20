0243d4a236e43c42ec0ddbaed6f00f5e70c7443b
philip993 <fitz993m@gmail.com>
2019-02-20 16:26:50 +0100

    comment model added

---
+++ models/Comm.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Post } = require("./Post");
+
+const commSchema = new mongoose.Schema({
+  bodyComment: {
+    type: String,
+    required: true
+  },
+  commentedOn: {
+    type: Date,
+    default: Date.now
+  },
+  linked: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+const Comm = mongoose.model("Comm", commSchema);
+
+exports.Comm = Comm;
